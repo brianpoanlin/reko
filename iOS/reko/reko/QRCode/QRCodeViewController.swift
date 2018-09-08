8543dd433127d3ce0b704c0606fd68c8562555db
pblin <pblin@umich.edu>
2018-09-07 23:28:53 -0400

    Programmatic QRCodeGeneratorVC

---
+++ iOS/reko/reko/QRCode/QRCodeViewController.swift
@@ -0,0 +1,67 @@
+//
+//  QRCodeViewController.swift
+//  reko
+//
+//  Created by Brian Lin on 9/7/18.
+//  Copyright © 2018 Brian Lin. All rights reserved.
+//
+
+import UIKit
+import QRCodeGenerator
+
+class QRCodeViewController: UIViewController {
+
+    fileprivate var qrCodeImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+    
+    public init() {
+        super.init(nibName: nil, bundle: nil)
+        self.view.backgroundColor = .white
+    }
+    
+    public convenience init(withContent: String) {
+        self.init()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        qrCodeImageView.image = RekoCodeGenerator.generatedQRCode(withCode: "brianlin.com", size: qrCodeImageView.frame.size)
+        view.addSubview(qrCodeImageView)
+
+        setUpConstraints()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
+    
+    private func setUpConstraints() {
+        qrCodeImageView.translatesAutoresizingMaskIntoConstraints = false
+        qrCodeImageView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        qrCodeImageView.heightAnchor.constraint(equalToConstant: 200).isActive = true
+        qrCodeImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        qrCodeImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
