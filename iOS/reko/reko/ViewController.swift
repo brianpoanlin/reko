f806c6e6ed31bada7fbc19439764cfd993f6ab72
pblin <pblin@umich.edu>
2018-09-08 00:50:11 -0400

    animation working

--- iOS/reko/reko/ViewController.swift
+++ iOS/reko/reko/ViewController.swift
@@ -14,20 +14,36 @@ class ViewController: UIViewController {
     private let qrcode = UIImageView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
     private var card = CardsView(viewModel: CardsViewModel())
     private var panGesture = UIPanGestureRecognizer()
+    private var swipeGesture = UISwipeGestureRecognizer()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         panGesture = UIPanGestureRecognizer(target: self, action: #selector(draggedView(_:)))
+        swipeGesture = UISwipeGestureRecognizer(target: self, action: #selector(swiped))
+        swipeGesture.direction = .up
         card.isUserInteractionEnabled = true
-        card.addGestureRecognizer(panGesture)
+//        card.addGestureRecognizer(panGesture)
+        card.addGestureRecognizer(swipeGesture)
     }
     
-    @objc func draggedView(_ sender:UIPanGestureRecognizer){
+    @objc func draggedView(_ sender: UIPanGestureRecognizer){
         self.view.bringSubview(toFront: card)
         let translation = sender.translation(in: self.view)
         card.center = CGPoint(x: card.center.x, y: card.center.y + translation.y)
         sender.setTranslation(CGPoint.zero, in: self.view)
+    }
+    
+    @objc func swiped(){
+        print("swipedup")
+        UIView.animate(withDuration: 0.5, animations: {
+            self.card.center = CGPoint(x: self.card.center.x, y: self.card.center.y - 600)
+            })
+//        self.view.bringSubview(toFront: card)
+//        let translation = sender.translation(in: self.view)
+//        card.center = CGPoint(x: card.center.x, y: card.center.y + translation.y)
+//        sender.setTranslation(CGPoint.zero, in: self.view)
     }
 
     
