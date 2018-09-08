6d49c525c4dc66c828b99fe94d0e37358a30322c
pblin <pblin@umich.edu>
2018-09-08 00:39:01 -0400

    card moving working

--- iOS/reko/reko/ViewController.swift
+++ iOS/reko/reko/ViewController.swift
@@ -13,10 +13,21 @@ class ViewController: UIViewController {
     
     private let qrcode = UIImageView(frame: CGRect(x: 100, y: 100, width: 200, height: 200))
     private var card = CardsView(viewModel: CardsViewModel())
+    private var panGesture = UIPanGestureRecognizer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        panGesture = UIPanGestureRecognizer(target: self, action: #selector(draggedView(_:)))
+        card.isUserInteractionEnabled = true
+        card.addGestureRecognizer(panGesture)
+    }
+    
+    @objc func draggedView(_ sender:UIPanGestureRecognizer){
+        self.view.bringSubview(toFront: card)
+        let translation = sender.translation(in: self.view)
+        card.center = CGPoint(x: card.center.x, y: card.center.y + translation.y)
+        sender.setTranslation(CGPoint.zero, in: self.view)
     }
 
     
