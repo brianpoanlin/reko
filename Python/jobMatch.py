f6d6740cb611db8ec12bde44fccc7da998196e0a
aganta <aganta@umich.edu>
2018-09-08 03:11:43 -0400

    jobMatch Poc

---
+++ Python/jobMatch.py
@@ -0,0 +1,20 @@
+import json
+
+def jobMatch(requestJson):
+    cardType = requestJson['type']
+    title = requestJson['title']
+    company = requestJson['Company']
+    fromDate = requestJson['From']
+    toDate = requestJson['To']
+    desc = requestJson['Desc']
+
+    returnJson = {
+        'cardType': cardType,
+        'title': title,
+        'company': company,
+        'fromDate': fromDate,
+        'toDate': toDate,
+        'desc': desc
+    }
+
+    return returnJson
