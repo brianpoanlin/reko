0d144c7e665cfc1abaa886aae59c0e17b8910a9d
pblin <pblin@umich.edu>
2018-09-07 21:05:49 -0400

    init empty ios project

---
+++ iOS/reko/reko/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  reko
+//
+//  Created by Brian Lin on 9/7/18.
+//  Copyright © 2018 Brian Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
